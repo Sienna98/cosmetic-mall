9e08fac571d888a7e2fec239a0e1f6d2ef04afdc
jihwan-kim3 <fankim@icloud.com>
2023-07-19 18:35:45 +0900

    chore : proxy settion

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/api:path*',
+        destination: 'localhost:3000/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
