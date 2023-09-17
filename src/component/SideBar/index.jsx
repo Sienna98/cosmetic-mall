52835bbb8eb5e10eb08eed872a79997644de00bb
sieun.lee <se.lee@purple.io>
2023-09-17 19:28:13 +0900

    연습하기

--- src/component/SideBar/index.jsx
+++ src/component/SideBar/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const SideBar = (productByBrand) => {
+const SideBar = (data) => {
+  console.log("data", data);
+
   return (
-    <ul>
-      {productByBrand.asdf.map((i) => {
-        return <li>{i.brand}</li>;
+    <ul style={{ width: "230px", padding: "40px", flexShrink: "0" }}>
+      {data.brandName.map((i) => {
+        return <li style={{ marginBottom: "8px" }}>{i.brand}</li>;
       })}
     </ul>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
-/* eslint-disable @next/next/no-img-element */
 import SideBar from "@/component/SideBar";
 import { useEffect, useState } from "react";
 
 const filterByBrand = (products) => {
   return [
+    // 중복된 브랜드 제거
     ...new Set(
       products
+        // brand 있는 배열만 걸러내기
         .filter((product) => product.brand)
+        // brand만 추출하여 새로운 배열 생성
         .map((product) => product.brand)
     ),
+    // 브랜드별로 제품 목록을 그룹화
   ].map((brand) => ({
     brand,
     products: products.filter((product) => product.brand === brand),
@@ -20,86 +23,107 @@ export default function Home() {
 
   useEffect(() => {
     fetch("/api/products")
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        // console.log("res", res);
-        const temp = filterByBrand(res).filter((item) => {
-          if (item.products.length > 10) {
-            return item;
+      .then((res) => res.json())
+      .then((data) => {
+        // 아이템이 10개 이상인 브랜드만 필터링
+        const temp = filterByBrand(data).filter((i) => {
+          if (i.products.length > 10) {
+            return i;
           }
         });
+        // 브랜드중 "dior" 찾아서 디올부터 3개 브랜드 삭제하기
         const indexOfDior = temp.findIndex((i) => i.brand === "dior");
         temp.splice(indexOfDior, 3);
-        // console.log(
-        //   "brand",
-        //   temp.map((i) => i.brand)
-        // );
+
         setProductByBrand(temp);
-      })
-      .catch((error) => {
-        // console.log("error", error);
       });
   }, []);
 
   return (
-    <div style={{ display: "flex", gap: "40px" }}>
-      <SideBar asdf={productByBrand} />
-      <div
+    <div style={{ display: "flex" }}>
+      <SideBar brandName={productByBrand} />
+      <section
         style={{
           display: "flex",
           flexDirection: "column",
-          gap: 10,
-          flexWrap: "nowrap",
+          alignItems: "center",
         }}
       >
-        {productByBrand?.map(({ brand, products }) => (
-          <div
-            key={brand}
-            style={{
-              padding: 10,
-              border: "1px solid #cccccc",
-              borderRadius: 10,
-              display: "flex",
-              flexDirection: "column",
-              gap: 30,
-            }}
-          >
-            <h1 style={{ fontSize: 22, fontWeight: 600 }}>{brand}</h1>
-
-            <div style={{ display: "flex", gap: 10, flexWrap: "wrap" }}>
-              {products.map((product) => (
-                <div
-                  key={product.id}
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <div
+        <div
+          style={{
+            padding: "20px 40px",
+            maxWidth: "1600px",
+          }}
+        >
+          {productByBrand &&
+            productByBrand.map((item) => {
+              return (
+                <>
+                  <h1
                     style={{
-                      width: 350,
-                      height: 350,
-                      border: "1px solid #dddddd",
+                      width: "100%",
+                      fontSize: "32px",
+                      fontWeight: "bold",
+                      color: "#333",
+                      paddingBottom: "15px",
                     }}
                   >
-                    <img
-                      src={product.image_link}
-                      alt={product.name}
-                      style={{ width: "100%", height: "100%" }}
-                    />
+                    {item.brand}
+                  </h1>
+                  <div
+                    key={item.id}
+                    style={{
+                      display: "flex",
+                      gap: "30px",
+                      flexWrap: "wrap",
+                      marginBottom: "100px",
+                    }}
+                  >
+                    {item.products.map((product) => {
+                      return (
+                        <div
+                          key={product.id}
+                          style={{
+                            width: "280px",
+                            height: "280px",
+                            boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                            borderRadius: "10px",
+                            padding: "10px",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            gap: "5px",
+                          }}
+                        >
+                          <div
+                            style={{
+                              width: "200px",
+                              height: "200px",
+                              overflow: "hidden",
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          >
+                            <img
+                              style={{
+                                objectFit: "cover",
+                              }}
+                              src={product.image_link}
+                              alt={product.name}
+                            />
+                          </div>
+                          <p>{product.name}</p>
+                        </div>
+                      );
+                    })}
                   </div>
-                  <span>
-                    {product.name} ${product.price}
-                  </span>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
+                </>
+              );
+            })}
+        </div>
+      </section>
     </div>
   );
 }
